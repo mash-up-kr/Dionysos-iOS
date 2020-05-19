8d2675e12ac0ee844a7280889f030e8415581001
Jercy <wotjdzz1@naver.com>
2020-05-19 18:23:51 +0900

    default setting

---
+++ Dionysos/SceneDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  SceneDelegate.swift
+//  Dionysos
+//
+//  Created by Jercy on 2020/05/19.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        print(scene)
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
---
+++ Dionysos/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Dionysos
+//
+//  Created by Jercy on 2020/05/19.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

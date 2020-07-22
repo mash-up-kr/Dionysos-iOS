8c8666cfe0e02df7bad4d7bf464763dd3770aabe
Jercy <wotjdzz1@naver.com>
2020-07-23 03:46:32 +0900

    Extensions들 추가

---
+++ Dionysos/Common/Extensions/UIView+Extension.swift
@@ -0,0 +1,92 @@
+//
+//  UIView+Border.swift
+//  Bluemoon
+//
+//  Created by 이재성 on 25/11/2018.
+//  Copyright © 2018 mashup6th. All rights reserved.
+//
+
+import UIKit
+
+class DismissSegue: UIStoryboardSegue {
+     override func perform() {
+          source.presentingViewController?.dismiss(animated: true, completion: nil)
+     }
+}
+
+extension UIView {
+     @IBInspectable var borderWidth: CGFloat {
+          get {
+               layer.borderWidth
+          }
+          set {
+               layer.borderWidth = newValue
+          }
+     }
+     
+     @IBInspectable var borderColor: UIColor? {
+          get {
+               guard let color: CGColor = layer.borderColor else { return nil }
+               return UIColor(cgColor: color)
+          }
+          set {
+               layer.borderColor = newValue?.cgColor
+          }
+     }
+}
+
+extension UIView {
+     @IBInspectable var cornerRadius: CGFloat {
+          get {
+               layer.cornerRadius
+          }
+          set {
+               layer.cornerRadius = newValue
+          }
+     }
+}
+
+extension UIView {
+     @IBInspectable var shadowRadius: CGFloat {
+          get {
+               layer.shadowRadius
+          }
+          set {
+               layer.shadowRadius = newValue
+          }
+     }
+     
+     @IBInspectable var shadowOpacity: Float {
+          get {
+               layer.shadowOpacity
+          }
+          set {
+               layer.shadowOpacity = newValue
+          }
+     }
+     
+     @IBInspectable var shadowOffset: CGSize {
+          get {
+               layer.shadowOffset
+          }
+          set {
+               layer.shadowOffset = newValue
+          }
+     }
+     
+     @IBInspectable var shadowColor: UIColor? {
+          get {
+               if let color: CGColor = layer.shadowColor {
+                    return UIColor(cgColor: color)
+               }
+               return nil
+          }
+          set {
+               if let color: UIColor = newValue {
+                    layer.shadowColor = color.cgColor
+               } else {
+                    layer.shadowColor = nil
+               }
+          }
+     }
+}

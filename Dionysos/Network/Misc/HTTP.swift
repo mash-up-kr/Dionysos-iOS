358d4d8db57f6564ae706801aea1c2625e1f5e57
이동영 <o.o.wl.soft@gmail.com>
2020-07-23 22:35:58 +0900

    feat: Network레이어 구현 🌐

---
+++ Dionysos/Network/Misc/HTTP.swift
@@ -0,0 +1,16 @@
+//
+//  HTTP.swift
+//  Dionysos
+//
+//  Created by dongyoung.lee on 2020/07/23.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum HTTP {
+    typealias Method = Moya.Method
+    typealias Headers = [String: String]
+    typealias Parameters = [String: Any?]
+}

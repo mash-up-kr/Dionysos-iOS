b3729f2ec1b53d8404dbfe28f4ef432854bf04c6
jae57 <ggapsong@naver.com>
2020-07-25 23:20:50 +0900

    add sign up view

--- Dionysos/SignUp/NicknameInputViewController.swift
+++ Dionysos/SignUp/NicknameInputViewController.swift
@@ -9,8 +9,42 @@
 import UIKit
 
 final class NicknameInputViewController: UIViewController, KeyboardConstraintHandler {
-    @IBOutlet private weak var confirmView: UIView!
-    @IBOutlet private weak var bottomConstraintOutlet: NSLayoutConstraint?
+    @IBOutlet private weak var nicknameTextField: UITextField!
+    @IBOutlet private weak var nicknameReviewLabel: UILabel!
+    @IBOutlet private weak var bottomConstraintOutlet: NSLayoutConstraint!
+    @IBOutlet private weak var confirmButton: UIButton!
+    
+    @IBAction func nicknameFieldChanged(_ sender: Any) {
+        // print(nicknameTextField.text)
+        // apicall
+        if true {
+            confirmButton.isSelected = true
+            // 응답을 화면에 보여줌
+            nicknameReviewLabel.text = "진짜 멋지다!"
+            nicknameReviewLabel.textColor = UIColor(named: "azure")
+            confirmButton.isEnabled = true
+        } else {
+            confirmButton.isSelected = false
+            confirmButton.isEnabled = false
+            nicknameReviewLabel.text = "음..........."
+            nicknameReviewLabel.textColor = UIColor(named: "coralPink")
+        }
+    }
+    
+    @IBAction func confirmButtonClicked(_ sender: Any) {
+        
+        // 닉네임 넣어서 회원가입 apiCall
+        
+        let vc = UIStoryboard.init(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: "NicknameSuccessViewController")
+        
+        guard let successViewController = vc as? NicknameSuccessViewController else { return }
+        successViewController.modalPresentationStyle = .fullScreen
+        self.present(successViewController, animated: true, completion: nil)
+        successViewController.setConfirmedNickname(nickname: "원숭이가나무에서떨어졌다")
+    }
+    
+    var keyboardIsShown: Bool = false
+    var baseConstraint: CGFloat = CGFloat(19)
     
     var bottomConstraint: NSLayoutConstraint? {
         bottomConstraintOutlet

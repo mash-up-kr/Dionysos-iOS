f6cc8af1b3dc2331471f5f2264fc72d3c6f96dd0
이동영 <o.o.wl.soft@gmail.com>
2020-07-25 18:57:47 +0900

    ✨feat: TimeAmount 구현

---
+++ Dionysos/Common/Timer/MGKTimer.swift
@@ -0,0 +1,13 @@
+//
+//  MGKTimer.swift
+//  Dionysos
+//
+//  Created by dongyoung.lee on 2020/07/25.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import Foundation
+import Promises
+
+class MGKTimer {
+}

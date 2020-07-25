15faa94cb0d41fd4837ad1ee482d27041ebd0454
Jihye Kim <ggapsong@naver.com>
2020-07-25 23:22:15 +0900

    add sign up view (#26)

--- Dionysos/Common/KeyboardConstraintHandler.swift
+++ Dionysos/Common/KeyboardConstraintHandler.swift
@@ -10,29 +10,39 @@ import UIKit
 
 protocol KeyboardConstraintHandler {
     var bottomConstraint: NSLayoutConstraint? { get }
+    var keyboardIsShown: Bool { get set }
+    var baseConstraint: CGFloat { get }
+    
     func keyboardReactive()
 }
 
 extension KeyboardConstraintHandler where Self: UIViewController {
     func keyboardReactive() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { [weak self] notification in
+            guard let isShown = self?.keyboardIsShown else { return }
+            if isShown {
+                return
+            }
             self?.addBottomConstraint(noti: notification as NSNotification)
         }
         
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) { [weak self] notification in
+            guard let isShown = self?.keyboardIsShown else { return }
+            if !isShown {
+                return
+            }
             self?.removeBottomConstraint(noti: notification as NSNotification)
         }
     }
     
-    func addBottomConstraint(noti: NSNotification) {
-        print("addBottom")
+    mutating func addBottomConstraint(noti: NSNotification) {
         guard let cgRect = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        bottomConstraint?.constant = cgRect.height
+        bottomConstraint?.constant = baseConstraint + cgRect.height
+        keyboardIsShown = true
     }
-    
-    func removeBottomConstraint(noti: NSNotification) {
-        print("removeBottom")
-        guard let cgRect = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        bottomConstraint?.constant = cgRect.height
+    // struct 애플 문서보기 -> mutating
+    mutating func removeBottomConstraint(noti: NSNotification) {
+        bottomConstraint?.constant = baseConstraint
+        keyboardIsShown = false
     }
 }
---
+++ Dionysos/SignUp/NicknameSuccessViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NicknameSuccessViewController.swift
+//  Dionysos
+//
+//  Created by 김지혜 on 2020/07/25.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import UIKit
+
+class NicknameSuccessViewController: UIViewController {
+    @IBOutlet private weak var confirmedNicknameLabel: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let nextVC = UIStoryboard.init(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: "")
+            self.present(nextVC, animated: true, completion: nil)
+        }
+    }
+    
+    func setConfirmedNickname(nickname: String) {
+        confirmedNicknameLabel?.text = nickname
+    }
+}

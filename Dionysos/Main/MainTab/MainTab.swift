bb02b1483fdf95edb75792e50251c6edd2d1d008
Jercy <wotjdzz1@naver.com>
2020-07-25 19:01:43 +0900

    이미지 에셋 추가

---
+++ Dionysos/Main/MainTab/MainTab.swift
@@ -0,0 +1,12 @@
+//
+//  MainTab.swift
+//  Dionysos
+//
+//  Created by Jercy on 2020/07/25.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import UIKit
+
+class MainTab: UIView {
+}
---
+++ Dionysos/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Dionysos
+//
+//  Created by Jercy on 2020/07/25.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

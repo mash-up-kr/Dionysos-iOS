42ab5440d28a57335126dfed6a2ad209e0bfe9de
이동영 <o.o.wl.soft@gmail.com>
2020-07-25 20:30:24 +0900

    ✨feat: TimerTask로직 구현

---
+++ Dionysos/Common/Timer/TimeTask.swift
@@ -0,0 +1,34 @@
+//
+//  TimeTask.swift
+//  Dionysos
+//
+//  Created by dongyoung.lee on 2020/07/25.
+//  Copyright © 2020 Mashup. All rights reserved.
+//
+
+import Foundation
+
+class TimeTask {
+    private var startDate: Date?
+    private(set) var endDate: Date?
+    var state: State = .todo {
+        didSet {
+            switch state {
+            case .todo:
+                return
+            case .inProgress:
+                self.startDate = Date()
+            case .done:
+                self.endDate = Date()
+            }
+        }
+    }
+    
+    enum State {
+        case todo
+        case inProgress
+        case done
+    }
+}
+
+func useCase() {}

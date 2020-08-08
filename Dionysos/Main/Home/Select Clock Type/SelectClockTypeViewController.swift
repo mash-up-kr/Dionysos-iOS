57d92fefa6138e4e18af18a895dff030ceee990f
Jercy <wotjdzz1@naver.com>
2020-08-08 15:29:10 +0900

    탭 이슈 수정

--- Dionysos/Main/Home/Select Clock Type/SelectClockTypeViewController.swift
+++ Dionysos/Main/Home/Select Clock Type/SelectClockTypeViewController.swift
@@ -26,7 +26,10 @@ final class SelectClockTypeViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        view.addSubview(MainTabCenter.default.getMainTab())
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
